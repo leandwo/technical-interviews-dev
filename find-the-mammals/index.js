63d75091ccea3882b70574f847ee401bbcfd88ac
Leandro Solidum <leandro@leandros-mbp.local>
2024-09-02 18:27:22 -0700

    start: find the mammals

---
+++ find-the-mammals/index.js
@@ -0,0 +1,6 @@
+function copyAnimalsToMammals() {
+    const animalsSection = document.getElementsByClassName("all-animals")
+    for (let animalDiv of animalsSection) {
+        const isMammal = animalDiv.childNodes
+    }
+}
